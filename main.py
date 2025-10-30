2d90d8726d22a61c671c4fccca2785f4ee0fe254
mustafomadazimov111-ops <mustafomadazimov111@gmail.com>
2025-10-30 09:24:45 -0400

    Update: audio language and unique filenames

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from deep_translator import GoogleTranslator
 from gtts import gTTS
 import os
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -30,7 +30,7 @@ def index():
 
     return render_template("index.html", translated_text=translated_text, audio_file=audio_file)
 
-if name == "main":
+if __name__ == "main":
     from os import environ
     port = int(environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
